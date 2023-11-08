25442fe6da261e9b826f0134a01364e8ad6cc087
erxide <erwan.sinck12@gmail.com>
2023-11-08 11:38:29 +0100

    ouai ouai

---
+++ Tp4Py/code/Simple_bs_program/bs_client_I2.py
@@ -0,0 +1,23 @@
+import socket
+
+host = "9.2.4.3"
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.connect((host, port))
+    print(f"Connecté avec succès au serveur {host} sur le port {port}")
+    user_input = input("Que veux-tu envoyer au serveur : ")
+    try:
+        s.send(user_input.encode())
+    except socket.error:
+        print("Erreur lors de l'envoi.")
+except socket.error:
+    print("La connexion a échoué.")
+
+data = s.recv(1024)
+
+s.close()
+
+print(f"Le serveur a répondu: {data.decode()}")
---
+++ Tp4Py/code/Simple_bs_program/bs_server_I2.py
@@ -0,0 +1,29 @@
+import socket
+
+host = ""
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))
+
+s.listen(1)
+
+conn , addr = s.accept()
+
+print('Un client vient de se co et son IP c\'est', addr, ".")
+
+while True:
+    
+    try:
+        data = conn.recv(1024)
+        if not data: break
+        if "meo" in data.decode : conn.send("Meo à toi confrère.".encode())
+        elif "waf" in data.decode : conn.send("ptdr t ki".encode())
+        elif not "waf" in data.decode or not "meo" in data.decode : conn.send("Mes respects humble humain.".encode())
+
+    except socket.error:
+        print("Error Occured.")
+        break
+
+conn.close()
