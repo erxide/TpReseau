e4e1a739706381cd75e0a1612e5b091d7fc6aaf1
erxide <erwan.sinck12@gmail.com>
2023-11-08 15:12:14 +0100

    Update bs_server_II2A.py

--- Tp4Py/code/You_say_dev_I_say_good_practices/bs_server_II2A.py
+++ Tp4Py/code/You_say_dev_I_say_good_practices/bs_server_II2A.py
@@ -16,6 +16,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind((host, port))
 logging.info(f"Le serveur tourne {host}:{port}.")
+print("\033[255m" + "INFO" + "\033[0m", f"Le serveur tourne sur {host}:{port}")
 
 s.listen(1)
 
@@ -23,6 +24,7 @@ s.listen(1)
 
 conn , addr = s.accept()
 logging.info(f'Un client {addr} vient de se co.')
+print("\033[255m" + "INFO" + "\033[0m", f"Un client {addr} vient de se co.")
 
 while True:
     
@@ -30,11 +32,13 @@ while True:
         data = conn.recv(1024)
         if not data: break
         logging.info(f"Le client {addr} a envoyé: {data.decode()}")
+        print("\033[255m" + "INFO" + "\033[0m", f"Le client {addr} a envoyé: {data.decode()}")
         if "meo" in data.decode() : message = "Meo à toi confrère." #conn.send("Meo à toi confrère.".encode())
         elif "waf" in data.decode() : message = "ptdr t ki" #conn.send("ptdr t ki".encode())
         elif not "waf" in data.decode() or not "meo" in data.decode : message = "Mes repects humble humain." #conn.send("Mes respects humble humain.".encode())
         conn.send(message.encode())
         logging.info(f"Réponse envoyée au client {addr} : {message}")
+        print("\033[255m" + "INFO" + "\033[0m", f"Réponse envoyée au client {addr} : {message}")
 
     except socket.error:
         print("Error Occured.")
