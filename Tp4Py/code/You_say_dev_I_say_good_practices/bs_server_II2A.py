d12257380d60ef41dbbc7965a0b17bc6f19bb75d
erxide <erwan.sinck12@gmail.com>
2023-11-08 15:08:54 +0100

    Update bs_server_II2A.py

--- Tp4Py/code/You_say_dev_I_say_good_practices/bs_server_II2A.py
+++ Tp4Py/code/You_say_dev_I_say_good_practices/bs_server_II2A.py
@@ -1,10 +1,16 @@
 import socket
 import logging
+import os
 
 host = "9.2.4.3"
 port = 13337
 
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%y-%m-%d %H:%M:%S')
+
+if not os.path.exists('/var/log/bs_server'):
+    os.makedirs('/var/log/bs_server')
+
+
+logging.basicConfig(filename='/var/log/bs_server/bs_server.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,6 +18,8 @@ s.bind((host, port))
 logging.info(f"Le serveur tourne {host}:{port}.")
 
 s.listen(1)
+
+
 
 conn , addr = s.accept()
 logging.info(f'Un client {addr} vient de se co.')
@@ -21,9 +29,12 @@ while True:
     try:
         data = conn.recv(1024)
         if not data: break
-        if "meo" in data.decode() : conn.send("Meo à toi confrère.".encode())
-        elif "waf" in data.decode() : conn.send("ptdr t ki".encode())
-        elif not "waf" in data.decode() or not "meo" in data.decode : conn.send("Mes respects humble humain.".encode())
+        logging.info(f"Le client {addr} a envoyé: {data.decode()}")
+        if "meo" in data.decode() : message = "Meo à toi confrère." #conn.send("Meo à toi confrère.".encode())
+        elif "waf" in data.decode() : message = "ptdr t ki" #conn.send("ptdr t ki".encode())
+        elif not "waf" in data.decode() or not "meo" in data.decode : message = "Mes repects humble humain." #conn.send("Mes respects humble humain.".encode())
+        conn.send(message.encode())
+        logging.info(f"Réponse envoyée au client {addr} : {message}")
 
     except socket.error:
         print("Error Occured.")
