b774715848b41066bbaa871df1795521b64ce695
erxide <erwan.sinck12@gmail.com>
2023-11-28 11:01:46 +0100

    ptn

--- Tp5py/noencodeclient.py
+++ Tp5py/noencodeclient.py
@@ -1,10 +1,15 @@
 import socket 
 
-host = "9.3.4.3"
+host = "9.2.4.3"
 port = 13337
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+
+try:
+    s.connect((host, port))
+except socket.error:
+    print("Impossible de se connecter au serveur.")
+    exit(1)
 
 s.send(1000)
 
