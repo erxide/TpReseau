2a7117a42493b1f3739da1f16836055dadb2c429
erxide <erwan.sinck12@gmail.com>
2023-11-28 11:08:01 +0100

    ntm

--- Tp5py/noencode.py
+++ Tp5py/noencode.py
@@ -7,7 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
 
-s.listen(5)
+s.listen(1)
 
 conn, addr = s.accept()
 print(f"Connexion de {addr}.")
