fe452babc91e735c335a275dde7eb3da59580f6c
erxide <erwan.sinck12@gmail.com>
2023-11-28 15:59:55 +0100

    Update noencode.py

--- Tp5py/noencode.py
+++ Tp5py/noencode.py
@@ -14,7 +14,7 @@ try :
     conn, addr = s.accept()
     print(f"Connexion de {addr}.")
     len_msg = conn.recv(1)
-    msg = conn.recv(int.from_bytes(decode(len_msg), 'big'))
+    msg = conn.recv(decode(len_msg))
     print(decode(msg))
 except socket.error:
     print(socket.error)
